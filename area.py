fa5c6d553a00ee55c8a2d35f2c87f139074edfbe
dalia <daliagarduno308@aragon.unam.mx>
2024-04-27 23:40:09 -0600

    area

---
+++ area.py
@@ -0,0 +1,18 @@
+print ("Este es un programa que calcula el área de figuras")
+print ("indica cuanto mide el lado del cuadrado")
+ladoCuadrado=input()
+areaCuadrado=float(ladoCuadrado)*float(ladoCuadrado)
+print ("el area del cuadrado es:")
+print (areaCuadrado)
+print ("ahora vamos a calcular el area del rectangulo")
+print ("indica cuanto mide lado base del rectangulo")
+ladoBase=input()
+print ("indica cuanto mide la altura")
+ladoAltura=input()
+areaRectangulo=float(ladoBase)*float(ladoAltura)
+print ("el area del rectangulo es:")
+print (areaRectangulo)
+
+
+
+
